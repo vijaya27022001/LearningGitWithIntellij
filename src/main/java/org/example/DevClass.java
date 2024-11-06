c20b38c06069ae91d73cef5cbe9ea3f233de8d02
vijaya27022001 <kolisettivijayalakshmi@gmail.com>
2024-11-06 15:57:44 +0530

    Added an comment

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev Class");
+        //This is my first line of code
     }
 }
